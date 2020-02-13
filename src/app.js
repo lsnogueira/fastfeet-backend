6fb1330e6a793189179aa4dd5b2ca5543c940c3a
Lucas Nogueira <lucas_snogueira@hotmail.com>
2020-02-13 01:05:44 -0200

    wip session route and controller

--- src/app.js
+++ src/app.js
@@ -1,2 +1,18 @@
 // ponto de start do meu app, chamar midlewares e routes com use
+import express from 'express';
+
 import './database';
+
+class App {
+  constructor() {
+    this.server = express();
+
+    this.middlewares();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+  }
+}
+
+export default App().server;
---
+++ src/routes.js
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+
+const routes = new Router();
+
+// routes.post('/session',  SessionController.store)
+
+module.exports = routes;
