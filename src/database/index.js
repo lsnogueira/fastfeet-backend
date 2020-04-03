e0b6bf969e2e3bcdc6bdedcf37d274facbba474c
Lucas Nogueira <lucas_snogueira@hotmail.com>
2020-04-03 02:05:53 -0300

    create file storage with multer

--- src/database/index.js
+++ src/database/index.js
@@ -1,11 +1,12 @@
 import Sequelize from 'sequelize';
 
 import User from '../app/models/User';
+import File from '../app/models/File';
 import Recipient from '../app/models/Recipient';
 
 import databaseConfig from '../config/database';
 
-const models = [User, Recipient];
+const models = [User, File, Recipient];
 
 class Database {
   constructor() {
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,16 @@
 import { Router } from 'express';
 
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/session', SessionController.store);
 
@@ -13,5 +18,7 @@ routes.use(authMiddleware);
 
 routes.post('/recipients', RecipientController.store);
 routes.put('/recipients', RecipientController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 module.exports = routes;
