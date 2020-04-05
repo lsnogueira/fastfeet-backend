916389a8a1832a3c51738978afdada20aede4d99
Lucas Nogueira <lucas_snogueira@hotmail.com>
2020-04-05 15:51:18 -0300

    add validation user exists, code enhancements and migration adjustments

--- src/app/controllers/CourierController.js
+++ src/app/controllers/CourierController.js
@@ -6,19 +6,33 @@ class CourierController {
   async store(req, res) {
     const schema = Yup.object().shape({
       name: Yup.string().required(),
-      email: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
     });
 
     if (!(await schema.isValid(req.body))) {
       return res.status(400).json({ message: 'Validation fails' });
     }
 
+    const userExists = await Courier.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (userExists) {
+      return res.status(400).json({ error: 'User already exists' });
+    }
+
     const courier = await Courier.create({
       name: req.body.name,
       email: req.body.email,
     });
-
-    return res.json(res.json(courier));
+    const { id, name, email } = courier;
+    return res.json({
+      id,
+      name,
+      email,
+    });
   }
 }
 
--- src/database/migrations/20200405150138-create-couriers.js
+++ src/database/migrations/20200405150138-create-couriers.js
@@ -16,6 +16,16 @@ module.exports = {
         allowNull: false,
         unique: true,
       },
+      avatar_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'files',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true,
+      },
       created_at: {
         type: Sequelize.DATE,
         allowNull: false,
